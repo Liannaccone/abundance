2f82f28b9e9c35f14b2f147443a0a81a4ad83a0c
liannaccone <lciannaccone@gmail.com>
2018-03-09 14:31:46 -0800

    fixing bug

--- models/useritem.js
+++ models/useritem.js
@@ -1,6 +1,6 @@
 
 module.exports = function(sequelize, Sequelize) {
-	var Useritem = sequelize.define('useritem', {
+	var Useritem = sequelize.define('Useritem', {
 		name: {
 			type: Sequelize.STRING,
 			allowNull: false
@@ -10,14 +10,14 @@ module.exports = function(sequelize, Sequelize) {
 			allowNull: false
 		}
 	});
-	// Useritem.associate = function(models) {
-	// 	// all items belong to a specific User
-	// 	// adding foreign key constraint so an Item cannot be created without a User
-	// 	Useritem.belongsTo(models.User, {
-	// 		foreignKey: {
-	// 			allowNull: false
-	// 		}
-	// 	});
-	// };
+	Useritem.associate = function(models) {
+		// all items belong to a specific User
+		// adding foreign key constraint so an Item cannot be created without a User
+		Useritem.belongsTo(models.User, {
+			foreignKey: {
+				allowNull: false
+			}
+		});
+	};
 	return Useritem;
 }
