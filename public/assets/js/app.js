2f5831a0c5c957c67083141f758a43d24c5af413
liannaccone <lciannaccone@gmail.com>
2018-03-12 13:36:09 -0700

    updating font and landing page format

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,20 +1,22 @@
 $(document).ready(function(){
 	
 	$('.add-button').on('click', function(event) {
-		var newProduct = {
-			product_id: $(this).parent().data('id')
-		}
-		// ajax call to /api/useritem' method='post'>
-	    $.ajax("/api/useritem", {
-	      type: "POST",
-	      data: newProduct
-	    }).then(
-	      function() {
-	        console.log("created new useritem");
-	        // Reload the page to get the updated list
-	        location.reload();
-	      }
-	    );
+		var newProduct = $(this).data('id')
+		// var newProduct = {
+		// 	product_id: $(this).data('id')
+		// }
+		alert('\n\n***********',newProduct)
+		// // ajax call to /api/useritem' method='post'>
+	    // $.ajax("/api/useritem", {
+	    //   type: "POST",
+	    //   data: newProduct
+	    // }).then(
+	    //   function() {
+	    //     console.log("created new useritem");
+	    //     // Reload the page to get the updated list
+	    //     location.reload();
+	    //   }
+	    // );
 	});
 
     $("[rel='tooltip']").tooltip();    
