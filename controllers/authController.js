913f99471c35eefddbe9b4134794af9a8796cd8d
Jordan Ketring <jordanketring@airbears2-10-142-148-24.airbears2.1918.berkeley.edu>
2018-03-12 19:33:58 -0700

    Add more styling

--- controllers/authController.js
+++ controllers/authController.js
@@ -65,7 +65,7 @@ var db = require("../models");
     // var hbsObject = {}
     // db.Post.findAll({}).then(function(data){
     //   dbsObject.post = data
-    //   res.render('forum', hbsObject);
+      res.render('forum');
     // })
   }
 
